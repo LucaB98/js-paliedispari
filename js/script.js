077656e48b7751c63ab7a21a703ba92ad5df6721
LucaB98 <lucabellan98@gmail.com>
2024-01-09 16:20:29 +0100

    added second es

--- js/script.js
+++ js/script.js
@@ -1,47 +1,49 @@
 console.log('JS OK')
 
-// Palidroma
-// Chiedere all’utente di inserire una parola (con un prompt)
-// Creare una funzione per capire se la parola inserita è palindroma
-// Stampiamo il risultato in console
-
-
-function isPalindrome(word){
-    
-    let inverseWord = '';
-
-    for(let i = word.length - 1; i >= 0; i--)
-    inverseWord += word[i];
-
-    if(inverseWord === word){
-        return true;
-    }else{
-        return false;
-    };
-    
-};
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. (con un prompt)
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto in console.
 
 
 // scaletta
-// 1 - chiedere all'utente la parola
-// 2 - creare la funzione
+// prendere gli elemnti interessati
+// 1 - chiedere all'utente pari o dispari e un numero da 1 a 5
+// 2 - creare la funzione per creare un numero random da 1 a 5
     // a - come la chiamo?
     // b - mi servono parametri?
     // c - devo restituire qualcosa?
     // d - se si, di che tipo?
-// 3 - stampare il risultato in colsole
+// 3 - sommare numero utente e numero pc
+// 4 - creare la funzione per stabilire pari o dispari 
+    // a - come la chiamo?
+    // b - mi servono parametri?
+    // c - devo restituire qualcosa?
+    // d - se si, di che tipo?
+// 5 - stampiamo in console(bonus in pagina)
 
-const inputField = document.getElementById('text');
+
+const selectElement = document.getElementById('select');
+const inputField = document.getElementById('usernumber');
 const button = document.getElementById('button');
-const resultElement = document.getElementById('result');
+
+
+
+
+function numberRandom(min = 1, max = 5) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
+
 
 button.addEventListener('click', function(){
-    const word = inputField.value;
-    resultElement.innerText = (isPalindrome(word))
+    const numberPc = numberRandom();
+    const numberUser = inputField.value;
+    let sum = 0;
+    sum = numberPc + numberUser;
+    console.log(sum)
+
 });
-
-
-
-
-    
-
